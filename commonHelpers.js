38e05b4ffe08b9dce57658e7cb657789d588322a
Subx1s0o <68860695+Subx1s0o@users.noreply.github.com>
2024-05-16 12:21:10 +0000

    Deploying to gh-pages from @ Subx1s0o/goit-js-hw-11@1c96e309574bd36d914ffce804fbcf32c50e4abb 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,6 +1,6 @@
-import{S as a,i as c}from"./assets/vendor-8c59ed88.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&l(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function l(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const n="43901454-2f0f1ad212df2deb6dd93021b",d=i=>fetch(`https://pixabay.com/api/?key=${n}&q=${i}&image_type=photo&orientation=horizontal&safesearch=true`).then(t=>{if(!t.ok)throw new Error("Помилка при отриманні запиту");return t.json()}).catch(t=>{throw console.error("Помилка при отриманні даних",t),t}),u=i=>{const t=document.querySelector(".list"),o=i.hits,l=document.querySelector(".loader-div");t.innerHTML="";let e="";const r=new a(".card-link",{inlineStyles:!1,captionsData:"alt",captionDelay:250,disableScroll:!0});o.length!==0?(l.style.display="flex",o.forEach(s=>{e+=`<li class="card">
-                <a class="card-link" href="${s.largeImageURL}">
-                    <img class="card-image" src="${s.webformatURL}" alt="${s.tags}" /> 
+import{S as n,i as d}from"./assets/vendor-0fc460d7.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function e(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(t){if(t.ep)return;t.ep=!0;const r=e(t);fetch(t.href,r)}})();const u="43901454-2f0f1ad212df2deb6dd93021b",h=i=>{const s=new URLSearchParams({image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`https://pixabay.com/api/?key=${u}&q=${i}&${s}`).then(e=>{if(!e.ok)throw new Error("Помилка при отриманні запиту");return e.json()}).catch(e=>{throw console.error("Помилка при отриманні даних",e),e})},c=document.querySelector(".list"),f=new n(".card-link",{inlineStyles:!1,captionsData:"alt",captionDelay:250,disableScroll:!0}),p=i=>{c.innerHTML="";let s="";i.forEach(e=>{s+=`<li class="card">
+                <a class="card-link" href="${e.largeImageURL}">
+                    <img  class="card-image" src="${e.webformatURL}" alt="${e.tags}" /> 
                 </a>
                 <div class="main-content">
                     <ul class="card-list">
@@ -8,27 +8,27 @@ import{S as a,i as c}from"./assets/vendor-8c59ed88.js";(function(){const t=docum
                             <h3>
                                 likes
                             </h3>
-                            <p>${s.likes}</p>
+                            <p>${e.likes}</p>
                         </li>
                         <li class="card-list-li">
                             <h3>
                                 views
                             </h3>
-                            <p>${s.views}</p>
+                            <p>${e.views}</p>
                         </li>
                         <li class="card-list-li">
                             <h3>
                                 comments
                             </h3>
-                            <p>${s.comments}</p>
+                            <p>${e.comments}</p>
                         </li>
                         <li class="card-list-li">
                             <h3>
                                 downloads
                             </h3>
-                            <p>${s.downloads}</p>
+                            <p>${e.downloads}</p>
                         </li>
                     </ul>
                 </div>
-            </li>`}),l.style.display="none",t.insertAdjacentHTML("beforeend",e),r.refresh()):c.show({title:"❌",message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"white",backgroundColor:"#E25757",position:"topRight"})},f=document.querySelector("form"),h=document.querySelector("input[data-search]");f.addEventListener("submit",i=>{i.preventDefault();const t=h.value;d(t).then(o=>{u(o)}).catch(o=>{console.error("Помилка при рендері картинок",o)})});
+            </li>`}),c.insertAdjacentHTML("beforeend",s),f.refresh()},m=document.querySelector("form"),y=document.querySelector("input[data-search]"),l=document.querySelector(".loader-div");m.addEventListener("submit",i=>{i.preventDefault(),l.style.visibility="visible";const s=y.value;h(s).then(e=>{const o=e.hits;o.length!==0?(p(o),l.style.visibility="hidden"):d.show({title:"❌",message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"white",backgroundColor:"#E25757",position:"topRight"})}).catch(e=>{console.error("Помилка при рендері картинок",e)})});
 //# sourceMappingURL=commonHelpers.js.map
